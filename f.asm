bd7110980bd3ea663d7fd76a8b571093306943d9
xX_ShakeDev[REAL]_Xx <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-03-29 18:06:27 -0300

    Update f.asm

--- f.asm
+++ f.asm
@@ -1,1 +1,15 @@
-test
+section .data
+    msg db 'Hello, world!', 0
+
+section .text
+    global _start
+
+_start:
+    ; Escrever a string na tela
+    mov ah, 09h         ; Código da função de serviço para escrever uma string
+    mov dx, msg         ; DX aponta para a string
+    int 21h             ; Chamada de interrupção 21h para imprimir a string
+
+    ; Encerrar o programa
+    mov ah, 4Ch         ; Código da função de serviço para terminar o programa
+    int 21h             ; Chamada de interrupção 21h para encerrar o programa
